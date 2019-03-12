26cfcbf75152039e862979a4d622977c27138d7b
Margela <32767638+Margela@users.noreply.github.com>
2019-03-12 18:43:27 +0000

    Add files via upload

---
+++ LoginPage.java
@@ -0,0 +1,56 @@
+package com.example.margelanoryaly.solophysio;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginPage  extends AppCompatActivity {
+
+
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    TextView mTextViewRegister;
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage);
+
+        db = new DatabaseHelper(this);
+
+
+        mTextUsername = (EditText) findViewById(R.id.edittext_Username);
+        mTextPassword = (EditText) findViewById(R.id.edittext_Password);
+        mButtonLogin = (Button) findViewById(R.id.button_login);
+        mTextViewRegister = (TextView) findViewById(R.id.textview_register);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginPage.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                Boolean res = db.checkUser(user, pwd);
+                if (res == true) {
+                    Toast.makeText(LoginPage.this, "Succesfully Logged In", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginPage.this, "Login Error", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+    }
+}
